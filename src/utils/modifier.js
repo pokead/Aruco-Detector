43d088aa4a6e0855fea1da42e52bec45545a7d22
leo <leo@example.com>
2024-02-22 13:26:00 +0100

    Switched canvas library to skia-canvas og & added image distortion

--- src/utils/modifier.js
+++ src/utils/modifier.js
@@ -1,11 +1,13 @@
-const { createCanvas, loadImage } = require('@napi-rs/canvas')
+//const { createCanvas, loadImage } = require('@napi-rs/canvas')
+const {Canvas, loadImage} = require('skia-canvas')
 const fs = require("fs")
 const sharp = require("sharp")
 
 
 async function modifyImage(width, height, markers, buffer) {
-  const canvas = createCanvas(width, height)
-  const ctx = canvas.getContext('2d')
+  const canvas = new Canvas(width, height),
+    
+    ctx = canvas.getContext('2d');
   const img = await loadImage(buffer)
   
   ctx.drawImage(img, 0, 0)
@@ -21,15 +23,47 @@ async function modifyImage(width, height, markers, buffer) {
 
     const img = await sharp(fs.readFileSync('./examples/gato.jpeg')).resize(imgOptions).toBuffer()
     const image = await loadImage(img)
+    const quad = [
+      marker.corners[0].x, marker.corners[0].y,
+      marker.corners[1].x, marker.corners[1].y,
+      marker.corners[2].x, marker.corners[2].y,
+      marker.corners[3].x, marker.corners[3].y
+    ]
 
     ctx.moveTo(marker.corners[0].x, marker.corners[0].y)
     marker.corners.forEach(corner => ctx.lineTo(corner.x, corner.y))
     ctx.lineTo(marker.corners[0].x, marker.corners[0].y)
+    const matrix = ctx.createProjection(quad)
+    ctx.setTransform(matrix)
     ctx.drawImage(image, marker.corners[0].x, marker.corners[0].y)
   }
 
   ctx.stroke()
-  return canvas.toBuffer('image/png').toString('base64')
+  const result = await canvas.png;
+  return result.toString('base64')//canvas.toBuffer('image/png').toString('base64')
 }
+
+
+
+
+
+
+function createProjection2(quad){
+  console.log([quad].flat())
+  let [a, b, c, d, e, f, p0, p1, p2] = quad
+  const matrix = new DOMMatrix(
+    [
+      a, d, 0, p0,
+      b, e, 0, p1,
+      0, 0, 1, 0,
+      c, f, 0, p2
+    ]
+
+  )
+  return matrix
+}
+
+
+
 
 module.exports = modifyImage
