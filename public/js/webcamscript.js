b101a77838a52c9dbba5af7f2865d32162637419
Leo <Leo>
2024-04-16 16:01:53 +0200

    Demo webcam completamente funzionante

--- public/js/webcamscript.js
+++ public/js/webcamscript.js
@@ -1,5 +1,6 @@
 let video = document.querySelector('video')
-let canvas = document.querySelector('canvas')
+let canvas = document.getElementsByClassName('real')[0]
+let fake = document.getElementsByClassName('fake')[0]
 
 if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
     console.log("Let's get this party started")
@@ -10,8 +11,17 @@ if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
             video.play()
             canvas.width = video.videoWidth
             canvas.height = video.videoHeight
+            canvas.getContext('2d').drawImage(
+                video, 0, 0, canvas.width, canvas.height)
+            fake.width = video.videoWidth
+            fake.height = video.videoHeight
+            fake.getContext('2d').drawImage(
+                video, 0, 0, fake.width, fake.height)
             setInterval(async function() {
-                canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+                //console.log(canvas.toDataURL('image/jpeg'))
+                fake.getContext('2d').drawImage(
+                    video, 0, 0, fake.width, fake.height)
+                //canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
                 //console.log(canvas.toDataURL('image/jpeg'))
                 //fetch(
                 //    "http://127.0.0.1:8000/image/?image="+(canvas.toDataURL('image/jpeg')),
@@ -20,8 +30,7 @@ if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
                 //    }
                 //).then(function (res) { console.log(res)})
                 //console.log("http://127.0.0.1:8000/image/?image="+encodeURI(canvas.toDataURL('image/jpeg')))
-                canvas.getContext('2d').drawImage(
-                    video, 0, 0, canvas.width, canvas.height)
+                
                 fetch('http://127.0.0.1:8000/image', {
                     method: 'POST',
                     headers: {
@@ -29,20 +38,22 @@ if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
                         //'Access-Control-Allow-Origin': "*"
                     },
                     //mode: "no-cors",
-                    body: JSON.stringify({image: canvas.toDataURL('image/jpeg')}),
+                    body: JSON.stringify({image: fake.toDataURL('image/jpeg')}),
                 }).then(response => {
                     return response.json();
                   }).then(jsonResponse => {
-                    console.log(jsonResponse.buffer);
+                    //console.log(jsonResponse.buffer);
                     const image = new Image();
                     image.src = jsonResponse.buffer;
-                    canvas.getContext('2d').drawImage(image, 0, 0, canvas.width, canvas.height);
-                    
+                    image.onload = function() {
+                        canvas.getContext('2d').drawImage(image, 0, 0, canvas.width, canvas.height);
+                    }
+
                   }).catch (error => {
                     console.log(error)
                   })
                 //console.log(await response.json())
-            }, 240)
+            }, 41)
 
         }
     })
